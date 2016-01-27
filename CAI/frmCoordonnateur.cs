ebe2d3ed52f4a797f14342042a2f0fe8445289f5
AntoineMonzerol <antoine00000@hotmail.com>
2016-01-27 14:31:04 -0500

    GUI
    -frmCoordonnateur : Correction des check box dans le tab Usagers (peut
    maintenant qu'en cocher un seul)

--- CAI/frmCoordonnateur.cs
+++ CAI/frmCoordonnateur.cs
@@ -19,7 +19,11 @@ namespace CAI
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            dataGridView1.Rows[e.RowIndex].Cells[2].Value = false;
+            dataGridView1.Rows[e.RowIndex].Cells[3].Value = false;
+            dataGridView1.Rows[e.RowIndex].Cells[4].Value = false;
 
+            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = true;
         }
     }
 }
