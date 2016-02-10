76a2a59e36db4c01aefe71ac8ef285c97b113647
GratMaster <grat_master_2@hotmail.com>
2016-02-10 09:50:56 -0500

    Création Usagers

--- CAI/frmCreation.cs
+++ CAI/frmCreation.cs
@@ -42,6 +42,33 @@ namespace CAI
                 MessageBox.Show("Votre mot de passe doit contenir au moins 8 caractères !");
             else if (!AdresseValide(txtNomUsager.Text))
                 MessageBox.Show("Le nom d'usager n'est pas une adresse valide !");
+            else
+            {
+                string[] paramIN = new string[5];
+                paramIN[0] = txtPrenom.Text;
+                paramIN[1] = txtNom.Text; 
+                paramIN[2] = txtNomUsager.Text;
+                paramIN[3] = txtMDP.Text;
+                paramIN[4] = "0";
+
+                bool[] paramOUT = new bool[5];
+                paramOUT[0] = false;
+                paramOUT[1] = false;
+                paramOUT[2] = false;
+                paramOUT[3] = false;
+                paramOUT[4] = true;
+                CExecuteur.ObtenirCExecuteur().ExecPs("spInsererUsager", ref paramIN, paramOUT);
+
+                if (paramIN[4] == "-1")
+                    MessageBox.Show("Le nom d'utilisateur n'est pas valide. Veuillez en choisir un nouveau.");
+                if (paramIN[4] == "1")
+                {
+                    MessageBox.Show("Succès !");
+                    this.Dispose();
+                    this.RefAFrmCreation.Show();
+                }
+                    
+            }
 
         }
 
